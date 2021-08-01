05ef8479ee0b18f7be52b204f87f974227c320e7
alinear-me <aline.monteiro100@gmail.com>
2021-07-31 21:18:21 -0300

    primeira e segunda aplicação em springboot

---
+++ main/java/com/generation28/Juntes/JuntesApplication.java
@@ -0,0 +1,13 @@
+package com.generation28.Juntes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication 
+public class JuntesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuntesApplication.class, args);
+	}
+
+}
---
+++ test/java/com/generation28/Juntes/JuntesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation28.Juntes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuntesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
